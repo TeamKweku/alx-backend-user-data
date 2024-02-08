b4440831de7b088a0e8a92a518ca0001603d035f
teamkweku <teamkweku@outlook.com>
2024-02-08 07:19:25 +0000

    class to accept a list of strings fields constructor argument

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module that implements a PII functions
 """
 from typing import List
 import re
+import logging
 
 
 patterns = {
@@ -22,3 +23,23 @@ def filter_datum(
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        method that formats a LogRecord
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return text
