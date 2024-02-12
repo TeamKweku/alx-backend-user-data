13904916caad70a048ac545d1f4cda0ba23091dc
teamkweku <teamkweku@outlook.com>
2024-02-12 11:53:32 +0000

    extracting user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,3 +61,31 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
+            
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
+        """
+        Extracts the user email and password from the
+        Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str):
+            The Base64 decoded value.
+
+        Returns:
+            (str, str): The user email and password if they're valid
+            (None, None) otherwise.
+    """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None  
