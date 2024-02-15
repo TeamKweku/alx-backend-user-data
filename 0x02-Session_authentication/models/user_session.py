ef0d8b27f44c7f9d64f6fcd2a3c850ca89f88e85
teamkweku <teamkweku@outlook.com>
2024-02-15 13:35:12 +0000

    pycodestyle fix

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,8 +10,7 @@ class UserSession(Base):
     """
 
     def __init__(self, *args: list, **kwargs: dict):
-        """Initializes a User session instance.
-        """
+        """Initializes a User session instance."""
         super().__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = kwargs.get("user_id")
+        self.session_id = kwargs.get("session_id")
