63eb333703a9ecaef91cfb9e12d638d50f32dc8e
teamkweku <teamkweku@outlook.com>
2024-02-15 10:18:04 +0000

    pycodestyle error

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,15 +10,14 @@ from models.user import User
 from api.v1.views import app_views
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def session_auth():
-    """_summary_
-    """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None or email == '':
+    """_summary_"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
-    if password is None or password == '':
+    if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
     users = User.search({"email": email})
     if not users or users == []:
@@ -26,21 +25,23 @@ def session_auth():
     for user in users:
         if user.is_valid_password(password):
             from api.v1.app import auth
+
             session_id = auth.create_session(user.id)
             resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
+            session_name = os.getenv("SESSION_NAME")
             resp.set_cookie(session_name, session_id)
             return resp
     return jsonify({"error": "wrong password"}), 401
 
 
-@app_views.route('/auth_session/logout',
-                 methods=['DELETE'], strict_slashes=False)
+@app_views.route("/auth_session/logout", methods=["DELETE"],
+                 strict_slashes=False)
 def logout():
     """
     for logging out user
     """
     from api.v1.app import auth
+
     if auth.destroy_session(request):
         return jsonify({}), 200
     abort(404)
