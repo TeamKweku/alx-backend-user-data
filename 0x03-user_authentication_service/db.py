764d142f8b4e3756083c61c7814351925b0ea2af
teamkweku <teamkweku@outlook.com>
2024-02-20 07:38:54 +0000

    NoResultFound error

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-"""DB module
+"""DB module.
 """
 from sqlalchemy import create_engine, tuple_
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class."""
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance."""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object."""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
@@ -40,14 +41,36 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Method that finds a user based on a set
-        of fields
-        """
-        if not kwargs:
-            raise InvalidRequestError
+        """Finds a user based on a set of filters."""
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = (
+            self._session.query(User)
+            .filter(tuple_(*fields).in_([tuple(values)]))
+            .first()
+        )
+        if result is None:
+            raise NoResultFound()
+        return result
 
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if not user:
-            raise NoResultFound
-        return user
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Updates a user based on a given id."""
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
+            else:
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
+        self._session.commit()
