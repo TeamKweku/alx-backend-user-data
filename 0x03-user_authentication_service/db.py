97d3586077eb09fc8b44e6765157ffb3022faa3c
teamkweku <teamkweku@outlook.com>
2024-02-20 07:27:59 +0000

    method that finds users based on set of filters

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,18 +45,10 @@ class DB:
         Method that finds a user based on a set
         of fields
         """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = (
-            self._session.query(User)
-            .filter(tuple_(*fields).in_([tuple(values)]))
-            .first()
-        )
-        if result is None:
-            raise NoResultFound()
-        return result
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
